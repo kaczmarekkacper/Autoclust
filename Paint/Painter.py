b3df8d916027cf189500baeaa8911c0d56beda6b
kaaczmarekkacper <kazzper791@gmail.com>
2021-05-03 11:10:52 +0200

    cos na koniec

--- Paint/Painter.py
+++ Paint/Painter.py
@@ -24,23 +24,28 @@ class Painter:
         self.__format_set()
         plt.scatter(self.formatted_set["x"], self.formatted_set["y"],
                     c=self.formatted_set["label"])
+        plt.savefig('ByLabel.png')
         plt.show()
 
-    def paint_points_by_prediction(self):
+
+    def paint_points_by_prediction(self, title):
         self.__format_set()
         plt.scatter(self.formatted_set["x"], self.formatted_set["y"],
                     c=self.formatted_set["prediction"])
+        plt.savefig(f'{title}.png')
         plt.show()
 
     @staticmethod
     def paint_delaunay(points, tri):
         plt.triplot(points[:, 0], points[:, 1], tri.simplices)
         plt.scatter(points[:, 0], points[:, 1], c=Colors.Colors["DARK_GREEN"])
+        plt.savefig('Delaunay.png')
         plt.show()
 
     @staticmethod
-    def paint_points_with_edges(points, edges):
+    def paint_points_with_edges(points, edges, title):
         for edge in edges:
             plt.plot([edge[0].x, edge[1].x], [edge[0].y, edge[1].y], c=Colors.Colors["BLUE"])
         plt.scatter(points[:, 0], points[:, 1], c=Colors.Colors["DARK_GREEN"])
+        plt.savefig(f'{title}.png')
         plt.show()
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from ImportData import ImportData
 from Point import Point
 from Paint import Painter
 from Autoclust import Autoclust
+import configparser
 
 if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config_file = "config.ini"
+    config.read(config_file)
     import_data = ImportData.ImportData()
-    template = "2d_dataset_{:d}.csv"
-    template2 = "chameleon_ds{:d}.csv"
-    which_data = 7
-    data_raw = import_data.get_data("Data/" + template.format(which_data))
+    path_to_data = config['DEFAULT']['path_to_data']
+    data_raw = import_data.get_data(path_to_data)
     points = []
     for row in data_raw:
         point = Point.Point()
@@ -18,7 +20,7 @@ if __name__ == '__main__':
 
     painter = Painter.Painter()
     painter.set_to_paint = points
-    painter.paint_points()
+    painter.paint_points_by_label()
 
     # phase 1
     algorithm = Autoclust.Autoclust()
@@ -34,17 +36,17 @@ if __name__ == '__main__':
     algorithm.calculate_relative_st_dev()
     algorithm.sort_edges()
     other_edges = algorithm.get_other_edges()
-    painter.paint_points_with_edges(algorithm.point_array, other_edges)
+    painter.paint_points_with_edges(algorithm.point_array, other_edges, "other_edges")
 
     # phase 2
     algorithm.create_clusters_by_other_edges()
-    painter.paint_points_by_prediction()
+    painter.paint_points_by_prediction("clusters")
 
     short_edges = algorithm.get_short_edges()
-    painter.paint_points_with_edges(algorithm.point_array, short_edges)
+    painter.paint_points_with_edges(algorithm.point_array, short_edges, "short_edges")
 
     algorithm.check_assign_by_short_edges()
-    painter.paint_points_by_prediction()
+    painter.paint_points_by_prediction("phase2")
 
     # phase 3
 
@@ -52,9 +54,8 @@ if __name__ == '__main__':
     algorithm.erase_second_order_long_edges()
     short_edges = algorithm.get_short_edges()
     other_edges = algorithm.get_other_edges()
-    painter.paint_points_with_edges(algorithm.point_array, short_edges + other_edges)
+    painter.paint_points_with_edges(algorithm.point_array, short_edges + other_edges, "short_edges_other_edges")
 
-    algorithm.erase_predictions()
-    algorithm.make_predictions()
-    painter.paint_points_by_prediction()
-    painter.paint_points_by_label()
+    algorithm.create_clusters_by_other_edges()
+    algorithm.check_assign_by_short_edges()
+    painter.paint_points_by_prediction("phase3")
